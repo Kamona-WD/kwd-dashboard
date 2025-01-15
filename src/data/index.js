32f32d367a80793271b9f83b7e8775428644c5e7
Kamona-WD <ak3849909@gmail.com>
2025-01-16 00:09:57 +0200

    wip

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,37 @@
 export default {
     title: 'K-WD Dashboard',
+
+    navigationLinks: [
+        {
+            title: 'Dashboard',
+            url: '/',
+            iconName: 'tabler--home',
+        },
+        {
+            title: 'Pages',
+            url: null,
+            iconName: 'tabler--circle',
+            links: [
+                {
+                    title: 'Blank',
+                    url: '/pages/blank.html',
+                },
+            ],
+        },
+        {
+            title: 'Authentication',
+            url: null,
+            iconName: 'tabler--shield',
+            links: [
+                {
+                    title: 'Login',
+                    url: '/auth/login.html',
+                },
+                {
+                    title: 'Register',
+                    url: '/auth/register.html',
+                },
+            ],
+        },
+    ]
 }
